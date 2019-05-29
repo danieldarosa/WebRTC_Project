15b44678e20d3915e1f619cc4e90102af5dc73c8
Daniel DA ROSA <daniel.darosa@etu.hesge.ch>
2019-05-29 20:33:13 +0200

    first commit

---
+++ javascript/config.js
@@ -0,0 +1,3 @@
+var SERVERADDRESS = 'http://129.194.185.47';
+var SERVERPORT = ':6106';
+var SERVERLIVE = '/live_labo_smg/test1/playlist.m3u8';
---
+++ javascript/lecteur_HLS.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+const SERVERLINK = SERVERADDRESS + SERVERPORT + SERVERLIVE;
+
+var video = document.getElementById('video');
+if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(SERVERLINK);
+    hls.attachMedia(video);
+}
+// hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+// When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+// This is using the built-in support of the plain video element, without using hls.js.
+// Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
+// white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
+else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = SERVERLINK;
+}
