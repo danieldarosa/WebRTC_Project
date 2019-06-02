897f93f993974b0104fa5e842244046ef3783793
Daniel DA ROSA <daniel.darosa@etu.hesge.ch>
2019-06-02 15:04:50 +0200

    Connect to signaling server working

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,21 @@
 require('./lecteur_HLS');
+
+//Connect to the signaling server
+var ws = new WebSocket('ws://localhost:9090');
+var STUN_Server = null;
+newConnexion = new RTCPeerConnection(STUN_Server);
+
+console.log("RTC connexion was created");
+
+//Signaling server listeners
+ws.onopen = function() {
+    console.log("Connected");
+}
+
+ws.onclose = function() {
+    console.log("Disconnected");
+}
+
+ws.onerror= function(err) {
+    console.log("Error : ", err);
+}
