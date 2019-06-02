61f1078a47dca5aeb40b8f928238bfb9d5de2265
Daniel DA ROSA <daniel.darosa@etu.hesge.ch>
2019-06-02 14:52:45 +0200

    Server created

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    port: 9090,
+});
+console.log("[SERVER LOG]Server is running...");
+
+wss.on('connection', function connection(ws) {
+    
+    ws.on('message', function incoming(message) {
+    
+        console.log('[SERVER LOG]received:', message);
+
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+});
