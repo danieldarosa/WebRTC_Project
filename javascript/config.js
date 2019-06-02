71d02c8a1af7a50f0c51484297bbe767a17f7f33
Daniel DA ROSA <daniel.darosa@etu.hesge.ch>
2019-06-02 15:20:23 +0200

    Created id hashed

--- javascript/config.js
+++ javascript/config.js
@@ -1,7 +1,12 @@
-
+var crypto = require('crypto');
 //Media server info
 var SERVERADDRESS = 'http://129.194.185.47';
 var SERVERPORT = ':6106';
 var SERVERLIVE = '/live_labo_smg/test1/playlist.m3u8';
 
 exports.media_server_address = SERVERADDRESS + SERVERPORT + SERVERLIVE;
+
+//Creating crypto id
+var current_date = (new Date()).valueOf().toString();
+var random = Math.random().toString();
+exports.id = crypto.createHash('sha256').update(current_date + random).digest('hex');
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
 require('./lecteur_HLS');
+var config = require('./config');
 
 //Connect to the signaling server
 var ws = new WebSocket('ws://localhost:9090');
@@ -10,6 +11,8 @@ console.log("RTC connexion was created");
 //Signaling server listeners
 ws.onopen = function() {
     console.log("Connected");
+    var message = parseToMessage('ping', 'Hi from Daniel');
+    sendtosignaling(message);
 }
 
 ws.onclose = function() {
@@ -18,4 +21,22 @@ ws.onclose = function() {
 
 ws.onerror= function(err) {
     console.log("Error : ", err);
+}
+
+ws.onmessage= function(message) {
+    console.log("Message recieved :", message);
+}
+
+//Sending message to signaling server
+function sendtosignaling(message) {
+    ws.send(JSON.stringify(message));
+}
+
+function parseToMessage(type, data) {
+    
+    return {
+        "type" : type,
+        "data" : data,
+        "id" : config.id
+    }
 }
